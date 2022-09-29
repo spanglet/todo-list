318d6f2391042a84d82c4b04a1507f3dc9765239
Garret Spanglet <garretspanglet1@gmail.com>
2022-09-28 23:02:25 -0700

    Cleaned up unnecessary files, added to .gitignore and updated README.

--- app/config.py
+++
@@ -1,8 +0,0 @@
-
-
-class Config:
-    DEBUG = True
-    SECRET_KEY='dev'
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqldb:root@127.0.0.1:3306/mysqldb"
-
-
--- launch_pdb.sh
+++
@@ -1,6 +0,0 @@
-
-
-CONTAINER_ID=$(sudo docker ps --filter "name=todo_web_1" -q)
-
-sudo docker container attach $CONTAINER_ID
-
